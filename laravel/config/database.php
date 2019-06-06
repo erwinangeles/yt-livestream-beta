ff63a28b60d44f8adf51d37a98901d8c7be842cb
erwinangeles <51410283+erwinangeles@users.noreply.github.com>
2019-06-05 21:15:24 -0500

    Update database.php

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -45,11 +45,11 @@ return [
 
 'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'db4free.net'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel_ytlivest'),
+            'database' => env('DB_DATABASE', 'heroku_bec0bb4fa75a937'),
             'username' => env('DB_USERNAME', 'laravel_root_ean'),
-            'password' => env('DB_PASSWORD', '$LYou$tube20192ewdas'),
+            'password' => env('DB_PASSWORD', 'a796ff5a'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
